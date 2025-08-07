156d2225be79694059d39efed0e4f0d6b6a5e4ca
Aryan <aryanshah@192.168.1.11>
2025-08-07 18:06:41 +0530

    Committing Handle Reversed or Invalid Ranges changes

--- test4.py
+++ test4.py
@@ -9,6 +9,15 @@ def basic_range(str,delimeters):
         for delimiter in delimeters:
             if delimiter in part:
                 start, end = map(int, part.split(delimiter))
+                if start > end:
+                    start, end = end, start
+                if start < 0 or end < 0:
+                    continue
+                if start == end:
+                    result.append(start)
+                else:
+                    if start < 0 or end < 0:
+                        continue
                 result.extend(range(start, end + 1))
                 break
         else:
@@ -20,5 +29,5 @@ def basic_range(str,delimeters):
 delimeters=['-', 'to', '..', '~']
 print(basic_range('1..3',delimeters))
 print(basic_range(', 1-3 , ,5',delimeters))
-print(basic_range(',22-24,,,,26-45,',delimeters))
+print(basic_range(',5-3,,,,26-45,',delimeters))
 print(basic_range('10 to 12',delimeters))
